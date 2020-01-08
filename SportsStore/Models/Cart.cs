a2aeeb8dc461ab57139298aa994fba6e0ea58fc3
anureshmp <anureshmp@gmail.com>
2020-01-09 07:58:14 +1100

    Implemented shopping cart. Stored cart values in the session

--- SportsStore/Models/Cart.cs
+++ SportsStore/Models/Cart.cs
@@ -13,7 +13,7 @@ namespace SportsStore.Models
     }
     public class Cart
     {
-        private List<CartLine> lineCollection = new List<CartLine>();
+        public List<CartLine> lineCollection = new List<CartLine>();
 
         public virtual void AddItem(Product product, int Quantity)
         {
