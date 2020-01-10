03154c7453fcf8afb293c39d3a3be940dd760618
anureshmp <anureshmp@gmail.com>
2020-01-10 16:05:01 +1100

    Added shopping cart icon on the navbar and the supporting class for navigation

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Infrastructure;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent:ViewComponent
+    {
+       
+        public IViewComponentResult Invoke()
+        {
+            return View(GetCart());
+        }
+
+        private Cart GetCart()
+        {
+            Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+            return cart;
+        }
+
+    }
+}
