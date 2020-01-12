d4f0fc5e4129ba9f5589f142f7f355abd916571a
anureshmp <anureshmp@gmail.com>
2020-01-12 12:01:31 +1100

    Implemented order functionality, added model, contoller and supporting classes

---
+++ SportsStore/Models/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace SportsStore.Models
+{
+    public class Order
+    {
+        [BindNever]
+        public int OrderID { get; set; }
+
+        [BindNever]
+        public ICollection<CartLine> Lines { get; set; }
+        [Required(ErrorMessage = "Please enter a name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a first enter line")]
+        public string Line1 { get; set; }
+        public string Line2 { get; set; }
+        public string Line3 { get; set; }
+
+        [Required(ErrorMessage = "Enter a city name")]
+        public int City { get; set; }
+        public int State { get; set; }
+        public int Zip { get; set; }
+        [Required(ErrorMessage = "Enter a country name")]
+        public int Country { get; set; }
+        public bool GiftWrap { get; set; }
+
+
+
+
+
+
+    }
+}
